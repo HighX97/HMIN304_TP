0daf94e89a5a24d1c299b845777d199385b96be7
HighX97 <loic.ortole@gmail.com>
2017-01-06 08:21:39 +0100

    PrettyPrintVisitor

--- HMIN304_TP_1_2/src/visitors/PrettyPrintVisitor.java
+++ HMIN304_TP_1_2/src/visitors/PrettyPrintVisitor.java
@@ -3,17 +3,29 @@ public class PrettyPrintVisitor
 {
 
 	private String print ="";
+	private int indent=0;
 
 	public void visitDirectory(Directory directory)
 	{
+		//Indentation
+		String tabulation="";
+		for (int i = 0 ; i < indent ; i++ )
+		{
+			tabulation+="\t";
+		}
+
 		print += directory.getName();
 		print +="\n";
 
+		indent++;
+		tabulation+="\t";
 		for (ElementStockage e : directory.elements)
 		{
-			print +="\t";
+			print +=tabulation;
 			e.accept(this);
 		}
+
+		indent--;
 	}
 
 	public void visitFile(File file)
